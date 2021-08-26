3f4f4203bb4a12c2e965f3d0c0620c51ac663749
KHYehor <khilchenko.yehor@gmail.com>
2021-08-26 15:40:14 +0300

    feat: handlepair event transform data

--- src/mappings/chainlink/aggregators/aggregator.template.tsx
+++ src/mappings/chainlink/aggregators/aggregator.template.tsx
@@ -1,5 +1,5 @@
 import { AnswerUpdated } from "../../../types/{{{name}}}/AggregatorInterface";
-import { Asset } from "../../../types/schema";
+import { Asset, Index, IndexAsset } from '../../../types/schema'
 
 export function handleAnswerUpdated(event: AnswerUpdated): void {
   let asset = Asset.load("{{{address}}}");
@@ -7,7 +7,21 @@ export function handleAnswerUpdated(event: AnswerUpdated): void {
     return;
   }
 
-  asset.basePrice = event.params.current.toBigDecimal();
+  let oldPrice = asset.basePrice;
+  let newPrice = event.params.current;
+
+  let indexes = asset.indexes;
+  for (let i = 0; i < indexes.length; i++) {
+    let indexAsset = IndexAsset.load(indexes[i].toString())
+    let index = Index.load(indexAsset.index);
+    index.basePrice = index.basePrice
+      .minus(indexAsset.weight.toBigDecimal().times(oldPrice))
+      .plus(indexAsset.weight.times(newPrice).toBigDecimal());
+    index.baseVolume = index.basePrice.times(index.indexCount.toBigDecimal());
+    index.save();
+  }
+
+  asset.basePrice = newPrice.toBigDecimal();
 
   asset.save();
 }
