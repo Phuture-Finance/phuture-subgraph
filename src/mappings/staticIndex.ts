97a64982e3217242d4e62585cf8ee136eeed21fb
Eugene Lepeico <eugenelepeico@gmail.com>
2021-08-31 11:57:55 +0300

    fix: use total supply in market cap

--- src/mappings/staticIndex.ts
+++ src/mappings/staticIndex.ts
@@ -65,13 +65,15 @@ export function handleIndexTransfer(event: TransferEvent): void {
     toUserIndex.save();
   }
 
+  let value = event.params.value.toBigDecimal().div(BigInt.fromI32(10).pow(index.decimals.toI32() as u8).toBigDecimal());
+
   let transferType: string;
   if (from.toHexString() == ADDRESS_ZERO) {
-    index.totalSupply = index.totalSupply.plus(event.params.value);
+    index.totalSupply = index.totalSupply.plus(value);
 
     transferType = "Mint";
   } else if (to.toHexString() == ADDRESS_ZERO) {
-    index.totalSupply = index.totalSupply.minus(event.params.value);
+    index.totalSupply = index.totalSupply.minus(value);
 
     transferType = "Burn";
   } else {
