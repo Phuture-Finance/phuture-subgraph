b766bbfba12c63597993f1ea2f3a52393be8b136
Vadym Popov <me@vpopov.org>
2021-12-20 14:45:27 +0200

    PF-536: add handler for vTokenTransfer event

--- subgraphs/mvp/src/mappings/phuture/vToken.ts
+++ subgraphs/mvp/src/mappings/phuture/vToken.ts
@@ -1,5 +1,20 @@
-import { UpdateDeposit } from '../../types/vToken/vToken';
-import { loadOrCreateVToken } from '../entities';
+// @ts-ignore
+import { UpdateDeposit, VTokenTransfer } from '../../types/vToken/vToken';
+import { loadOrCreateVToken, loadVToken } from '../entities';
+
+export function handlerVTokenTransfer(event: VTokenTransfer): void {
+  let fromVToken = loadVToken(event.params.from);
+  if (fromVToken) {
+    fromVToken.platformTotalSupply -= event.params.amount;
+    fromVToken.save();
+  }
+
+  let toVToken = loadVToken(event.params.to);
+  if (toVToken) {
+    toVToken.platformTotalSupply += event.params.amount;
+    toVToken.save();
+  }
+}
 
 export function handlerUpdateDeposit(event: UpdateDeposit): void {
   let vt = loadOrCreateVToken(event.address);
