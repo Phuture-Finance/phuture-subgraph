49f83ca0ff5616b8e71d67d9098d24f6d4f92b77
Josip Koncurat <jkoncura@gmail.com>
2023-03-15 14:39:37 +0100

    fix: remove UniV3

--- subgraphs/helpers/src/consts.ts
+++ subgraphs/helpers/src/consts.ts
@@ -18,13 +18,7 @@ export const FeeAUM = 'AUM';
 
 // keccak256("ASSET_ROLE")
 export const ASSET_ROLE = Bytes.fromHexString(
-    '86d5cf0a6bdc8d859ba3bdc97043337c82a0e609035f378e419298b6a3e00ae6',
-);
-export const UNI_V3_ORACLE_ROLE = Bytes.fromHexString(
-    'e227202e31a2a70f89eef3dc7c41fbf4edfae8bf01acf40e719faad1ec8b6e61',
-);
-export const UNI_V3_PATH_ORACLE_ROLE = Bytes.fromHexString(
-    '95023b7bb0ba8cd791f7a72c9586613d786af77ed60f735edce7bc48cd0aff4d',
+  '86d5cf0a6bdc8d859ba3bdc97043337c82a0e609035f378e419298b6a3e00ae6',
 );
 
-export const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
